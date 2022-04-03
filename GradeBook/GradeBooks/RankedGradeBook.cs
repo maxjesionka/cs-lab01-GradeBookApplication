cd476b9ab49994da7a6e55a58770a44b2bf49664
maxjesionka <maksymilian.jesionka@microsoft.wsei.edu.pl>
2022-04-03 16:06:02 +0200

    10/24 RankedGradeBook

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            if (averageGrade > 80)
+                return 'A';
+            else if (averageGrade < 80 && averageGrade >= 60)
+                return 'B';
+            else if (averageGrade >= 40 && averageGrade < 60)
+                return 'C';
+            else if (averageGrade >= 20 && averageGrade < 40)
+                return 'D';
+            else
+                return 'F';
+        }
+      
+
+    }
+}
